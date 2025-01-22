908641988a18502599bc65adf314669b55c62588
Krissa Jiyani <krissajiyani020@gmail.com>
2025-01-22 22:34:46 +0530

    Add files via upload

---
+++ sales.py
@@ -0,0 +1,3 @@
+gross=float(input("Enter gross sales:"))
+net = gross -(10/100*gross)
+print("Net sales=",net)
---
+++ swap.py
@@ -0,0 +1,4 @@
+a=float(input("Enter a:"))
+b=float(input("Enter b:"))
+a,b=b,a
+print("a=",a,"b=",b)
