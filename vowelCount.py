6b58213eaeb5661571914c95859989a54fd9efbd
Krissa Jiyani <krissajiyani020@gmail.com>
2025-01-23 15:56:51 +0530

    Add files via upload

---
+++ vowelCount.py
@@ -0,0 +1,4 @@
+#number of vowels
+s = input("Enter a string")
+vowel = s.count("a") + s.count("A") + s.count("e") + s.count("E") + s.count("i") + s.count("I") + s.count("o") + s.count("O") + s.count("u") + s.count("U")
+print("Number of vowels:",vowel)
